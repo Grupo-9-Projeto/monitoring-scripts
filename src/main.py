3a4f4889af27ec86898ca692822cd9b41ffab52f
marceloo27 <marcelo.carvalho@sptech.school>
2026-08-20 12:43:08 -0300

    Feat: adicionando keyboardinterrupt para quando for fechar

--- src/main.py
+++ src/main.py
@@ -13,13 +13,17 @@ with open('dados_maquina.csv', 'w', newline='') as csvfile:
 
 print("iniciando")
 
-while(True):
-    coleta = relatorio()
-    with open('dados_maquina.csv', 'a', newline='') as csvfile:
+try:
+    while(True):
+        coleta = relatorio()
+        with open('dados_maquina.csv', 'a', newline='') as csvfile:
 
-        csv.writer(csvfile, delimiter=',').writerow([coleta[0]] + coleta[1] + [coleta[2]] + [coleta[3]] + [coleta[4]] + [coleta[5]] + [coleta[6]] + [coleta[7]])
-        cpu_nucleos = coleta[1]
-        cpus_individuais = " | ".join([f"cpu {i+1}: {cpu_nucleos[i]}%" for i in range(len(cpu_nucleos))])
-        print(f"CPU TOTAL: {coleta[0]}% | {cpus_individuais} | RAM: {coleta[2]}% | Disco: {coleta[3]}% | Quando foi: {coleta[4]} | Rede recebida: {coleta[5]} Mbps | Rede enviada: {coleta[6]} Mbps | Frequencia: {coleta[7]} MHz")
+            csv.writer(csvfile, delimiter=',').writerow([coleta[0]] + coleta[1] + [coleta[2]] + [coleta[3]] + [coleta[4]] + [coleta[5]] + [coleta[6]] + [coleta[7]])
+            cpu_nucleos = coleta[1]
+            cpus_individuais = " | ".join([f"cpu {i+1}: {cpu_nucleos[i]}%" for i in range(len(cpu_nucleos))])
+            print(f"CPU TOTAL: {coleta[0]}% | {cpus_individuais} | RAM: {coleta[2]}% | Disco: {coleta[3]}% | Quando foi: {coleta[4]} | Rede recebida: {coleta[5]} Mbps | Rede enviada: {coleta[6]} Mbps | Frequencia: {coleta[7]} MHz")
 
-    time.sleep(5)
+        time.sleep(5)
+except  KeyboardInterrupt:
+    print("encerrado")
+   
