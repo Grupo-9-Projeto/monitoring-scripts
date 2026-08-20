907d1aee3ddfdde5fd54a33165bb3b4d26d0695b
marceloo27 <marcelo.carvalho@sptech.school>
2026-08-20 09:21:16 -0300

    Feat: adicionando print e uso por cpu nele

--- src/main.py
+++ src/main.py
@@ -13,8 +13,12 @@ with open('dados_maquina.csv', 'w', newline='') as csvfile:
 print("iniciando")
 
 while(True):
+    coleta = relatorio()
     with open('dados_maquina.csv', 'a', newline='') as csvfile:
 
         csv.writer(csvfile, delimiter=',').writerow(relatorio())
+        cpu_nucleos = coleta[1]
+        cpus_individuais = " | ".join([f"cpu {i+1}: {cpu_nucleos[i]}%" for i in range(len(cpu_nucleos))])
+        print(f"CPU TOTAL: {coleta[0]}% | {cpus_individuais} | RAM: {coleta[2]}% | Disco: {coleta[3]}% | Quando foi: {coleta[4]} | Rede recebida: {coleta[5]} | Rede enviada: {coleta[6]} | Frequencia: {coleta[7]}")
 
     time.sleep(5)
