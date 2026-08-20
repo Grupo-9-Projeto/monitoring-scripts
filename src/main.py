b6cf0020d583404ee59a40d9f418a3c7c8ba115d
marceloo27 <marcelo.carvalho@sptech.school>
2026-08-20 12:22:40 -0300

    Feat: adicionando unidade de medida

--- src/main.py
+++ src/main.py
@@ -20,6 +20,6 @@ while(True):
         csv.writer(csvfile, delimiter=',').writerow([coleta[0]] + coleta[1] + [coleta[2]] + [coleta[3]] + [coleta[4]] + [coleta[5]] + [coleta[6]] + [coleta[7]])
         cpu_nucleos = coleta[1]
         cpus_individuais = " | ".join([f"cpu {i+1}: {cpu_nucleos[i]}%" for i in range(len(cpu_nucleos))])
-        print(f"CPU TOTAL: {coleta[0]}% | {cpus_individuais} | RAM: {coleta[2]}% | Disco: {coleta[3]}% | Quando foi: {coleta[4]} | Rede recebida: {coleta[5]} | Rede enviada: {coleta[6]} | Frequencia: {coleta[7]}")
+        print(f"CPU TOTAL: {coleta[0]}% | {cpus_individuais} | RAM: {coleta[2]}% | Disco: {coleta[3]}% | Quando foi: {coleta[4]} | Rede recebida: {coleta[5]} Mbps | Rede enviada: {coleta[6]} Mbps | Frequencia: {coleta[7]} MHz")
 
     time.sleep(5)
