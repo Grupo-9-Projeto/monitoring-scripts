b009620bbd903afed7b9421470a833ce145d197d
marceloo27 <marcelo.carvalho@sptech.school>
2026-08-21 15:28:51 -0300

    Fix: corrigindo erro no delimitador

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ if not os.path.exists('dados_maquina.csv'):
     with open('dados_maquina.csv', 'w', newline='') as csvfile:
         qtd = psutil.cpu_count(logical=True)
         # cpus = [f"cpu{i+1}(%)" for i in range(qtd)]
-        csv.writer(csvfile, delimiter=';').writerow(["cpu total(%)"] + ["cpus_nucleos"] + [ "ram(%)", "disco(%)", "Quando foi Coletado", "Rede recebida(Mbps)", "Rede enviada(Mbps)", 'Frequencia de uso da CPU(MHz)', "Endereco MAC"])
+        csv.writer(csvfile, delimiter=',').writerow(["cpu total(%)"] + ["cpus_nucleos"] + [ "ram(%)", "disco(%)", "Quando foi Coletado", "Rede recebida(Mbps)", "Rede enviada(Mbps)", 'Frequencia de uso da CPU(MHz)', "Endereco MAC"])
 
 
 print("iniciando")
@@ -20,7 +20,7 @@ try:
         coleta = relatorio()
         with open('dados_maquina.csv', 'a', newline='') as csvfile:
 
-            csv.writer(csvfile, delimiter=';').writerow([coleta[0]] + [coleta[1]] + [coleta[2]] + [coleta[3]] + [coleta[4]] + [coleta[5]] + [coleta[6]] + [coleta[7]] + [coleta[8]])
+            csv.writer(csvfile, delimiter=',').writerow([coleta[0]] + [coleta[1]] + [coleta[2]] + [coleta[3]] + [coleta[4]] + [coleta[5]] + [coleta[6]] + [coleta[7]] + [coleta[8]])
             cpu_nucleos = coleta[1]
             # cpus_individuais = " | ".join([f"cpu {i+1}: {cpu_nucleos[i]}%" for i in range(len(cpu_nucleos))])
             print(f"CPU TOTAL: {coleta[0]}% | {cpu_nucleos} | RAM: {coleta[2]}% | Disco: {coleta[3]}% | Quando foi: {coleta[4]} | Rede recebida: {coleta[5]} Mbps | Rede enviada: {coleta[6]} Mbps | Frequencia: {coleta[7]} MHz | MAC: {coleta[8]}")
