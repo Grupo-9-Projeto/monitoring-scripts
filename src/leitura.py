30c52b53cdcab3398e770440aea2e798c2c8b80c
marceloo27 <marcelo.carvalho@sptech.school>
2026-08-31 13:12:22 -0300

    Feat: adicionando 1 dia como variavel

--- src/leitura.py
+++ src/leitura.py
@@ -6,6 +6,7 @@ df.columns = df.columns.str.strip()
 df['Quando foi Coletado'] = pd.to_datetime(df['Quando foi Coletado'])
 ultima = df['Quando foi Coletado'].max()
 uma_hora =  df[df['Quando foi Coletado'] >= (ultima - pd.Timedelta(hours=1))]
+um_dia =  df[df['Quando foi Coletado'] >= (ultima - pd.Timedelta(hours=24))]
 
 
 
@@ -17,3 +18,4 @@ print(f"Média CPU última hora: {media_cpu_1h:.2f}%")
 
 pico_cpu_1h = uma_hora['cpu total(%)'].max()
 print(f"Pico CPU última hora: {pico_cpu_1h:.2f}%")
+
