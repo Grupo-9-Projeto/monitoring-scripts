47e90cf04efb4851bc85d3e07622b0b8bb77599e
marceloo27 <marcelo.carvalho@sptech.school>
2026-09-04 12:47:29 -0300

    Fix: corrigindo pra função o arquivo de leitura

--- src/leitura.py
+++ src/leitura.py
@@ -9,37 +9,41 @@ uma_hora =  df[df['Quando foi Coletado'] >= (ultima - pd.Timedelta(hours=1))]
 um_dia =  df[df['Quando foi Coletado'] >= (ultima - pd.Timedelta(hours=24))]
 
 
+def metricas():
+    media_ram_1h = uma_hora['ram(%)'].mean()
+    # print(f"Média RAM última hora: {media_ram_1h:.2f}%")
 
-media_ram_1h = uma_hora['ram(%)'].mean()
-print(f"Média RAM última hora: {media_ram_1h:.2f}%")
+    media_cpu_1h = uma_hora['cpu total(%)'].mean()
+    # print(f"Média CPU última hora: {media_cpu_1h:.2f}%")
 
-media_cpu_1h = uma_hora['cpu total(%)'].mean()
-print(f"Média CPU última hora: {media_cpu_1h:.2f}%")
-
-pico_cpu_1h = uma_hora['cpu total(%)'].max()
-print(f"Pico CPU última hora: {pico_cpu_1h:.2f}%")
-
-
-max = 0
-cpu_maior = ""
-for i in df.columns:
-    if i.lower().startswith('cpu') and 'total' not in i.lower():
-        media_atual = uma_hora[i].mean()
-
-        if media_atual > max:
-            max = media_atual
-            cpu_maior = i
+    pico_cpu_1h = uma_hora['cpu total(%)'].max()
+    # print(f"Pico CPU última hora: {pico_cpu_1h:.2f}%")
 
 
+    max = 0
+    cpu_maior = ""
+    for i in df.columns:
+        if i.lower().startswith('cpu') and 'total' not in i.lower():
+            media_atual = uma_hora[i].mean()
 
-print(f"CPU COM A MAIOR MÉDIA NA ULTIMA HORA: {cpu_maior} MÉDIA: {max}%")
+            if media_atual > max:
+                max = media_atual
+                cpu_maior = i
 
 
-download_hora = uma_hora['Rede recebida(Mbps)'].mean()
-upload_hora = uma_hora['Rede enviada(Mbps)'].mean()
-# perda_hora = uma_hora['perda pacotes'].mean()
 
-print(f"Download médio (1h): {download_hora:.2f} Mbps")
-print(f"Upload médio (1h): {upload_hora:.2f} Mbps")
-# print(f"Perda média de pacotes (1h): {perda_hora:.2f}%")
+    # print(f"CPU COM A MAIOR MÉDIA NA ULTIMA HORA: {cpu_maior} MÉDIA: {max}%")
 
+
+    download_hora = uma_hora['Rede recebida(Mbps)'].mean()
+    upload_hora = uma_hora['Rede enviada(Mbps)'].mean()
+    # perda_hora = uma_hora['perda pacotes'].mean()
+
+    
+    # print(f"Perda média de pacotes (1h): {perda_hora:.2f}%")
+
+    return[media_cpu_1h, media_ram_1h, pico_cpu_1h, cpu_maior,  download_hora, upload_hora]
+
+
+
+print(f" MEDIA CPU 1 HORA: {metricas()[0]:.2f} \n MEDIA RAM 1 HORA: {metricas()[1]:.2f} \n PICO CPU 1 HORA: {metricas()[2]} \n CPU MAIOR: {metricas()[3]} \n DOWNLOAD HORA: {metricas()[4]:.2f} \n UPLOAD HORA: {metricas()[5]}")
