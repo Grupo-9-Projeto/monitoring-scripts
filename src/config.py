1e3d1704bb787c0da95677c346f6f3c50334243f
marceloo27 <marcelo.carvalho@sptech.school>
2026-09-13 16:54:14 -0300

    Feat: adicionando o find env para achar o env no diretorio

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os  
 import mysql.connector
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
+load_dotenv(find_dotenv())
 
 cnx = {
     'host': os.getenv('DB_HOST'),
